49bffbe38f4844bcd69142e9b9357ad4986bebd9
mgallet <flanker@19pouces.net>
2017-01-28 00:14:27 +0100

    installation bug fixes

--- run_gunicorn.py
+++ run_gunicorn.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-from djangofloor.scripts import gunicorn
-os.environ['DF_CONF_NAME'] = 'updoc-gunicorn'
-gunicorn()
+from djangofloor.scripts import aiohttp
+os.environ['DF_CONF_NAME'] = 'updoc-aiohttp'
+aiohttp()
--- setup.py
+++ setup.py
@@ -4,10 +4,21 @@
 
 import codecs
 import os.path
-
+import re
+import sys
 from setuptools import setup, find_packages
-from updoc import __version__ as version
+__author__ = 'Matthieu Gallet'
 
+
+commands = filter(lambda x: x[0:1] != '-', sys.argv)
+
+
+# avoid a from pythonnest import __version__ as version
+# (that compiles pythonnest.__init__ and is not compatible with bdist_deb)
+version = None
+for line in codecs.open(os.path.join('updoc', '__init__.py'), 'r', encoding='utf-8'):
+    matcher = re.match(r"""^__version__\s*=\s*['"](.*)['"]\s*$""", line)
+    version = version or matcher and matcher.group(1)
 
 # get README content from README.md file
 with codecs.open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as fd:
@@ -35,7 +46,7 @@ setup(
     description='Upload HTML documentations and share bookmarks',
     long_description=long_description,
     author='Matthieu Gallet',
-    author_email='flanker@19pouces.net',
+    author_email='github@19pouces.net',
     license='CeCILL-B',
     url='https://github.com/d9pouces/Updoc',
     entry_points=entry_points,
@@ -45,8 +56,10 @@ setup(
     test_suite='updoc.tests',
     install_requires=requirements,
     setup_requires=[],
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-    ],
+    classifiers=['Operating System :: POSIX :: BSD', 'Operating System :: POSIX :: Linux',
+                 'Operating System :: Unix',
+                 'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
+                 'Programming Language :: Python :: 3.2', 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4', 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3 :: Only'],
 )
